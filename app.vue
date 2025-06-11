7e5e8b61250ad2fe0c0943b7e70d9cbc86d60f66
Imran Hossen <mu.imran2002@gmail.com>
2025-06-11 10:40:17 +0600

    Added 2.5% padding on each side on container (root)

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <!-- app.vue -->
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto" style="padding-left: 2.5%; padding-right: 2.5%;">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
