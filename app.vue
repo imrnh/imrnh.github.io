c4753462a2501ae87fa0a8c87fad7101d9cbb04f
Imran Hossen <mu.imran2002@gmail.com>
2025-06-11 08:53:10 +0600

    First Commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+   <div class="bg-blue-600 min-h-screen text-white flex items-center justify-center text-4xl">
+      Tailwind works if blue bg 🎉
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  // Static target (important for GitHub Pages)
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
+
+  // Set base path if you're deploying to GitHub project page (e.g., github.com/username/repo-name)
+  router: {
+    base: 'https://imrnh.github.io' // Replace with your actual repo name
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxt/scripts'
+  ]
+})
